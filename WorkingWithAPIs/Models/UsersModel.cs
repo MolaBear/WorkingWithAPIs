edc39add2271ae0d7875f3a887b5a63b2b20b723
Thembinkosi Mtsweni <thembinkosim@betsoftware.com>
2023-09-21 10:06:51 +0200

    model improvements
    Model validation is crucial to ensure that the data being sent to the API is valid and adheres to the expected format.
    Added the Required attribute to all properties to ensure they are provided.
    Used the StringLength attribute to set maximum lengths for the properties.
    Used the EmailAddress attribute to validate the email format.
    Used the Phone attribute to validate the phone number format.

--- WorkingWithAPIs/Models/UsersModel.cs
+++ WorkingWithAPIs/Models/UsersModel.cs
@@ -1,13 +1,31 @@
-﻿namespace WorkingWithAPIs.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WorkingWithAPIs.Models
 {
     public class UsersModel
     {
         public int Id { get; set; }
-        public string firstName { get; set; }
-        public string lastName { get; set; }
-        public string email { get; set; }
-        public string telephone { get; set; }
-        public string identityNumber { get; set; }
 
+        [Required(ErrorMessage = "First name is required.")]
+        [StringLength(50, ErrorMessage = "First name cannot be longer than 50 characters.")]
+        public string firstName { get; set; }
+
+        [Required(ErrorMessage = "Last name is required.")]
+        [StringLength(50, ErrorMessage = "Last name cannot be longer than 50 characters.")]
+        public string lastName { get; set; }
+
+        [Required(ErrorMessage = "Email is required.")]
+        [EmailAddress(ErrorMessage = "Invalid email format.")]
+        [StringLength(100, ErrorMessage = "Email cannot be longer than 100 characters.")]
+        public string email { get; set; }
+
+        [Required(ErrorMessage = "Telephone is required.")]
+        [Phone(ErrorMessage = "Invalid phone number format.")]
+        [StringLength(15, ErrorMessage = "Telephone number cannot be longer than 15 characters.")]
+        public string telephone { get; set; }
+
+        [Required(ErrorMessage = "Identity number is required.")]
+        [StringLength(20, ErrorMessage = "Identity number cannot be longer than 20 characters.")]
+        public string identityNumber { get; set; }
     }
 }
